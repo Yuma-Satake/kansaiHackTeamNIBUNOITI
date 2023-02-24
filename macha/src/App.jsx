1f07e35cc83b4a0adc5ba5ca57d59eae05875c1f
Yuma Satake <satous.y@icloud.com>
2023-02-24 20:06:56 +0900

    テスト撤去

--- macha/src/App.jsx
+++ macha/src/App.jsx
@@ -2,7 +2,6 @@ import PageRouter from "./router/PageRouter";
 import CssBaseline from "@mui/material/CssBaseline";
 import { ThemeProvider } from "@mui/system";
 import mainTheme from "./style/Theme";
-import PostMailpreTest from "./components/preTest/postMailpreTest";
 
 function App() {
   return (
