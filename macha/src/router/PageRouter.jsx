f5463bdcc89933a71a10b189aa753e3879c9cc82
Yuma Satake <satous.y@icloud.com>
2023-02-25 04:43:00 +0900

    ルーター仮修正

--- macha/src/router/PageRouter.jsx
+++ macha/src/router/PageRouter.jsx
@@ -6,40 +6,39 @@ import MailsPage from "../components/Pages/MailsPage";
 import ThredPage from "../components/Pages/ThredPage";
 import ErrorPage from "../components/Pages/ErrorPage";
 import { onAuthStateChanged } from "firebase/auth";
-import { useState, useEffect } from 'react';
-import { auth } from '../util/firebase';
-
+import { useState, useEffect } from "react";
+import { auth } from "../util/firebase";
 
 const PageRouter = () => {
-
   const [isLogin, setIsLogin] = useState(false);
 
   const checkLogin = () => {
-    onAuthStateChanged(auth, async(user) => {
+    onAuthStateChanged(auth, async (user) => {
       if (user) {
-        console.log('signed in');
-        localStorage.setItem("token", (await user.getIdToken()));
+        console.log("signed in");
+        localStorage.setItem("token", await user.getIdToken());
         localStorage.setItem("uid", user.uid);
-        
+
         setIsLogin(true);
       }
     });
-
   };
 
   useEffect(() => {
     checkLogin();
-  }, [])
-  
+  }, []);
 
-  return (  
+  return (
     <BrowserRouter>
       <Routes>
-        {/* <Route path={"/"} element={false? <MailsPage />: <Login /> }/> */}
-        <Route path={"/"} element={isLogin? <MailsPage />: <Login /> }/>
-        <Route path={"/MailsPage"} element={isLogin? <MailsPage />: <Login />} />
-        <Route path={"/ChatPage"} element={isLogin? <ChatPage />: <Login />} />
-        <Route path={"/ThredPage"} element={isLogin? <ThredPage />: <Login />} />
+        {/* <Route path={"/"} element={true ? <MailsPage /> : <Login />} />
+        <Route path={"/MailsPage"} element={true ? <MailsPage /> : <Login />} />
+        <Route path={"/ChatPage"} element={true ? <ChatPage /> : <Login />} />
+        <Route path={"/ThredPage"} element={true ? <ThredPage /> : <Login />} /> */}
+        <Route path={"/"} element={isLogin ? <MailsPage /> : <Login />} />
+        <Route path={"/MailsPage"} element={isLogin ? <MailsPage /> : <Login />} />
+        <Route path={"/ChatPage"} element={isLogin ? <ChatPage /> : <Login />} />
+        <Route path={"/ThredPage"} element={isLogin ? <ThredPage /> : <Login />} />
         <Route path={"/*"} element={<ErrorPage />} />
       </Routes>
     </BrowserRouter>
