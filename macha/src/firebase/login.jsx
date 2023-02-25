7081750e5851c4b849fc4eea07ebc20e5d302234
yuto-prgm <otuy822k@gmail.com>
2023-02-26 08:00:27 +0900

    画像パスの変更

--- macha/src/firebase/login.jsx
+++ macha/src/firebase/login.jsx
@@ -66,7 +66,7 @@ function Login() {
   return(
     <fragment>
       <Toolbar>
-      <Box component="img" alt='MaCha' src='./src/components/image/MaCha.png' width={90} sx={{mt:-3,mb:-2}}/>
+      <Box component="img" alt='MaCha' src='./src/assets/logo.png' width={90} sx={{mt:-3,mb:-2}}/>
       </Toolbar>
       <Box
         sx={{
@@ -76,7 +76,7 @@ function Login() {
         }}
       >
         <Box sx={title}>
-          <Box component="img" alt='MaCha' src='./src/components/image/woman.png' width={300} align='right'/>
+          <Box component="img" alt='MaCha' src='./src/assets/human.png' width={300} align='right'/>
           <Grid sx={{ml:5}}>
             <Grid sx={test}>
               <Box>社会人がよく使うMail。</Box>
@@ -94,7 +94,7 @@ function Login() {
         </Box>
         <Stack direction='row' alignItems='center'>
           <Grid sx={{paddingTop:"50px"}}>
-            <Box component="img" alt="1" src="./src/components/image/1Star.png" width={120} sx={{ml:10}}/>
+            <Box component="img" alt="1" src="./src/assets/1Star.png" width={120} sx={{ml:10}}/>
             <Box sx = {saleComplaints}>
               <Box align="center">メールを送信したけど、打ち間違いに後から気づいた……</Box>
               <Box align="center" fontWeight={"Bold"} >それ、MaChaなら解決できます!!!</Box>
@@ -103,7 +103,7 @@ function Login() {
           </Grid>
         </Stack>
         <Grid sx={{paddingTop:"50px",ml:"300px"}}>
-          <Box component="img" alt="2" src="./src/components/image/2Star.png" width={120} sx={{ml:10}}/>
+          <Box component="img" alt="2" src="./src/assets/2Star.png" width={120} sx={{ml:10}}/>
           <Box sx = {saleComplaints} >
             <Box align="center">メールを送信したけど、読んでくれたかどうか分からない……</Box>
             <Box align="center"fontWeight={"Bold"}>それ、MaChaなら解決できます!!!</Box>
@@ -111,7 +111,7 @@ function Login() {
           </Box>
         </Grid>
         <Grid sx={{paddingTop:"50px"}}>
-          <Box component="img" alt="3" src="./src/components/image/3Star.png" width={120} sx={{ml:10}}/>
+          <Box component="img" alt="3" src="./src/assets/3Star.png" width={120} sx={{ml:10}}/>
             <Box sx = {saleComplaints}>
             <Box align="center">メールを確認したいけど、履歴をさかのぼるのが大変……</Box>
             <Box align="center"fontWeight={"Bold"}>それ、MaChaなら解決できます!!!</Box>
