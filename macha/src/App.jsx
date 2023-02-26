6c543c19a7320b0e6082e00aabd12acffffb63c9
Yuma Satake <satous.y@icloud.com>
2023-02-26 13:41:49 +0900

    ログイン動線変更

--- macha/src/App.jsx
+++ macha/src/App.jsx
@@ -3,7 +3,6 @@ import CssBaseline from "@mui/material/CssBaseline";
 import { ThemeProvider } from "@mui/system";
 import mainTheme from "./style/Theme";
 import { createContext, useState } from "react";
-import Login2 from "./firebase/login2";
 
 export const UserInfoContext = createContext({});
 export const MailListContext = createContext({});
@@ -21,7 +20,6 @@ function App() {
           <CssBaseline />
           <ThemeProvider theme={mainTheme}>
             <PageRouter />
-            <Login2 />
           </ThemeProvider>
         </UserInfoContext.Provider>
       </MailListContext.Provider>
